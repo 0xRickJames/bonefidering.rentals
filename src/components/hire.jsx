9181336034ecae4392afa6ecad888b73d6fb8574
unknown <0xrickjames@gmail.com>
2023-06-12 23:30:28 -0400

    Update hire.jsx

--- src/components/hire.jsx
+++ src/components/hire.jsx
@@ -8,7 +8,7 @@ export const Hire = () => {
         <div className="section-title">
           <h2>Hire a Crew</h2>
           <p>
-          At Bonefide Ring Rentals, we understand that setting up a wrestling ring can be a complex and time-consuming task. That's why we offer professional ring crew members to help you with the setup of your own wrestling ring. Our experienced crew is well-versed in handling various types of rings and equipment, ensuring a seamless installation process. Whether you're a wrestling promotion, a training facility, or an individual wrestler needing assistance, our dedicated crew members will arrive promptly and efficiently assemble your ring to the highest standards. Take the stress out of the setup and let our experts handle it for you. Contact us today to discuss your ring crew needs and secure their services for your next event.
+          At Bonefide Ring Rentals, we understand that setting up a wrestling ring can be a complex and time-consuming task. That's why we offer professional ring crew members to help you with the setup of your own wrestling ring. Our experienced crew is well-versed in handling various types of rings and equipment, ensuring a seamless installation process. Whether you're a wrestling promotion, a training facility, or trying to setup a ring at your own house, our dedicated crew members will arrive promptly and efficiently assemble your ring to the highest standards. Take the stress out of the setup and let our experts handle it for you. Contact us today to discuss your ring crew needs and secure their services for your next event.
           </p>
         </div>
       </div>
